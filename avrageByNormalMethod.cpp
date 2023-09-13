3b30399a3373b5462719d4d263fe317b83a38d3f
MANISH KUMAR PRAJAPATI <71719780+manish353730@users.noreply.github.com>
2023-09-13 22:10:34 +0530

    Add files via upload

---
+++ avrageByNormalMethod.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	cout<<"ENTER THE 5 SUBJECT MARKS "<<endl;
+	cin>>a>>b>>c>>d>>e;
+	float avg= (float)((a+b+c+d+e)/5);
+	cout<<"Average is "<<avg;
+	return 0;
+}
+
