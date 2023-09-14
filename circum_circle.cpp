6c2f4b0b3457d9ddcda9b8e4ed32d661aaa7cf1a
MANISH KUMAR PRAJAPATI <71719780+manish353730@users.noreply.github.com>
2023-09-14 08:57:32 -0700

    Add files via upload

---
+++ circum_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+const float pi=3.142f;
+int main()
+{
+	int r;
+	cout<<"Enter Radius"<<endl;
+	cin>>r;
+	float result= (2*pi*r);
+	cout<<"Circumference of circle is "<<result;
+	return 0;
+}
---
+++ const_Constrr.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class student{
+	int roll_no, fee ;
+	char name[50];
+	public:
+		student()
+		{
+			cout<<"Enter Name : "<<endl;
+			cin>>name;
+			cout<<"Enter Roll No. : "<<endl;
+			cin>> roll_no;
+			cout<<"Enter fee : "<<endl;
+			cin>>fee;
+		}
+		void display() const
+		{
+			cout<<"Enter Name : "<<name<<endl;
+			cout<<"Enter Roll No. : "<<roll_no<<endl;
+			cout<<"Enter fee : "<<fee<<endl;
+		}
+		int getroll_no(int a) const
+		{
+			return a;
+		}
+		void setroll_no(int r)
+		{
+			roll_no = r;
+		}
+};
+	
+	
+	
+
+int main()
+{
+	const student s1;
+	s1.display();
+	student s2;
+	s2.display();
+	s2.setroll_no(7);
+	s2.display();
+}
